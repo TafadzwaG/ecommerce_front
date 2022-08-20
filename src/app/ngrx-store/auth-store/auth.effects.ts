f6a8b2d18507d18c50076de7132a13997342a3cc
TafadzwaG <tafadzwagashirah@gmail.com>
2022-08-20 12:44:19 +0200

    handleauthentication helper function

--- src/app/ngrx-store/auth-store/auth.effects.ts
+++ src/app/ngrx-store/auth-store/auth.effects.ts
@@ -1,3 +1,4 @@
+import { AuthResponseData } from './../../interfaces/auth-response';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Actions, ofType, Effect } from '@ngrx/effects';
@@ -8,5 +9,32 @@ import { HttpClient } from '@angular/common/http';
 import * as AuthActions from './auth.actions'
 import { User } from 'src/app/models/user.model';
 
+
+const handleAuthentication = (
+    id: number,
+    name : string,
+    email : string,
+    token: string,
+    cart: {
+      id: number,
+      items : []
+    },
+    wishlist: {
+      id: number,
+      items : []
+    }
+
+) => {
+    const user = new User(id, name, email, token, wishlist, cart)
+    localStorage.setItem('userData', JSON.stringify(user))
+    return new AuthActions.AuthenticateSuccess({
+        id: id,
+        name: name,
+        email: email,
+        token: token,
+        cart: cart,
+        wishlist : wishlist
+    })
+}
 
 
