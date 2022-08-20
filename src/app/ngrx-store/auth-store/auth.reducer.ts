7c7d10ad7423df29e82010fdec536b7f6146dc5d
TafadzwaG <tafadzwagashirah@gmail.com>
2022-08-20 14:44:59 +0200

    change strict to false in tsconfig.js for central store to work

--- src/app/ngrx-store/auth-store/auth.reducer.ts
+++ src/app/ngrx-store/auth-store/auth.reducer.ts
@@ -8,7 +8,7 @@ export interface State {
     loading: boolean
 }
 
-const initialState: State = {
+export const initialState: State = {
     user: null,
     authError: '',
     loading: false
@@ -17,7 +17,7 @@ const initialState: State = {
 export function authReducer(
     state = initialState,
     action: AuthActions.AuthActions
-){
+) :State {
     switch (action.type) {
         case AuthActions.AUTHENTICATE_SUCCESS:
             const user = new User(
@@ -30,32 +30,37 @@ export function authReducer(
             )
             return {
                 ...state,
-                authError: null,
+                authError: '',
                 user: user,
                 loading: false
             };
-        
-            case AuthActions.LOGOUT:
-                return {
-                    ...state,
-                    user: null
-                }
-            case AuthActions.LOGIN_START:
-            case AuthActions.SIGNUP_START:
-                return {
-                    ...state,
-                    authError: null,
-                    loading: true
-                };
-            case AuthActions.AUTHENTICATE_FAIL:
-                return {
-                    ...state,
-                    user: null,
-                    authError: action.payload,
-                    loading: false
-                }
-    
+
+        case AuthActions.LOGOUT:
+            return {
+                ...state,
+                user: null
+            }
+        case AuthActions.LOGIN_START:
+        case AuthActions.SIGNUP_START:
+            return {
+                ...state,
+                authError: '',
+                loading: true
+            };
+        case AuthActions.AUTHENTICATE_FAIL:
+            return {
+                ...state,
+                user: null,
+                authError: action.payload,
+                loading: false
+            }
+
         default:
-            return state;
+            return {
+                ...state
+            }
     }
+
+    
+    
 }
