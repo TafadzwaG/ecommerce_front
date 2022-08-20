6a699448d5c9deb09a79355f397b0f0464f31862
TafadzwaG <tafadzwagashirah@gmail.com>
2022-08-21 00:10:23 +0200

    added cart actions

---
+++ src/app/ngrx-store/cart-store/cart.actions.ts
@@ -0,0 +1,52 @@
+import { Action } from '@ngrx/store'
+
+export const ADD_ITEM_TO_CART_START = '[Cart] Add item to cart Start'
+export const REMOVE_ITEM_FROM_CART = '[Cart] Remove item from cart'
+export const ADDING_ITEM_FAILED = '[Cart] Failed to add item to cart'
+export const ADDING_ITEM_SUCCESS = '[Cart] Addinng item success'
+export const REMOVING_ITEM_SUCCESS = '[Cart] Removing item success'
+export const REMOVING_ITEM_FAILED = '[Cart] Removing item failed'
+
+
+
+export class AddItemToCartStart implements Action {
+    readonly type = ADD_ITEM_TO_CART_START
+
+    constructor(
+        public payload: {}
+    ){}
+}
+
+export class RemoveItemFromCart implements Action {
+    readonly type = REMOVE_ITEM_FROM_CART
+}
+
+export class AddingItemFailed implements Action {
+    readonly type = ADDING_ITEM_FAILED
+
+    constructor(
+        public payload: string
+    ){}
+}
+
+export class AddItemSuccess implements Action {
+    readonly type = ADDING_ITEM_SUCCESS
+    constructor(
+        public payload: {}
+    ){}
+}
+
+export class RemoveItemSuccess implements Action {
+    readonly type = REMOVING_ITEM_SUCCESS
+    constructor(
+        public payload : string
+    ){}
+}
+
+export class RemoveItemFailed implements Action {
+    readonly type = REMOVING_ITEM_FAILED
+
+    constructor(
+        public payload: string
+    ){}
+}
